33058bea7c57f5ff1c735a227f905836bbca671d
Zuhriddin <zuhriddin.ibn.zoir.ibn.kenja@gmail.com>
2024-11-23 13:33:15 +0500

    homework fayllari

---
+++ homework/no.js
@@ -0,0 +1,6 @@
+const n = 10; // Massiv uzunligi
+let arr = [];
+for (let i = 0; i < n; i++) {
+    arr.push(2 * i + 1); // Toq sonlarni qo'shish
+}
+console.log("Toq sonlardan tashkil topgan massiv:", arr.join(", "));
---
+++ masala.js
@@ -0,0 +1,5 @@
+const x = 12345; // Misol uchun bir butun son  
+const str = x.toString(); // Sonni satrga aylantirish  
+
+console.log(str); // Natija: "12345"  
+console.log(typeof str); // Natija: "string"
